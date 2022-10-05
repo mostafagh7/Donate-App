1c11fe347dfdb1fa2192e04657ecf49e3e40ff2b
mostafagh7 <101651869+mostafagh7@users.noreply.github.com>
2022-10-05 11:53:01 +0300

    Add files via upload

---
+++ lib/data/lists/blood_banks.dart
@@ -0,0 +1,32 @@
+import '../medical_center.dart';
+
+const bloodBanks = [
+  MedicalCenter(
+    name: "مركز باسل الأسد الطبي",
+    phoneNumbers: ["01372802", "01372803", "01372804", "01372805"],
+    location: "Damascus",
+    latitude: "33.52240072566439",
+    longitude: "36.29432589446037",
+  ),
+  MedicalCenter(
+    name: "بنك الدم المزة",
+    phoneNumbers: ["01444102"],
+    location: "Damascus",
+    latitude: "33.513616096035165",
+    longitude: "36.26166069975341",
+  ),
+  MedicalCenter(
+    name: "بنك الدم",
+    phoneNumbers: ["09931750"],
+    location: "Damascus",
+    latitude: "33.571527372034346",
+    longitude: "36.39688133585211",
+  ),
+  MedicalCenter(
+    name: "بنك الدم المركزي",
+    phoneNumbers: ["04524164"],
+    location: "Damascus",
+    latitude: "33.4987507683228",
+    longitude: "36.245994654284175",
+  ),
+];
---
+++ lib/data/lists/hospitals.dart
@@ -0,0 +1,60 @@
+import '../medical_center.dart';
+
+const hospitals = [
+  MedicalCenter(
+    name: "مشفى الشرق",
+    phoneNumbers: ["01350000", "01340460", "01374374", "01374444"],
+    location: "Damascus",
+    latitude: "33.51894065824084",
+    longitude: "36.29559915055157",
+  ),
+  MedicalCenter(
+    name: "المشفى العربي الجديد",
+    phoneNumbers: ["01636000"],
+    location: "Damascus",
+    latitude: "33.52005949627811",
+    longitude: "36.29804860558958",
+  ),
+  MedicalCenter(
+    name: "مشفى الأسد الجامعي",
+    phoneNumbers: ["01830000"],
+    location: "Damascus",
+    latitude: "33.52240072566439",
+    longitude: "36.29432589446037",
+  ),
+  MedicalCenter(
+    name: "مشفى الأماني الحديثة",
+    phoneNumbers: ["01340626"],
+    location: "Damascus",
+    latitude: "33.522561723063156",
+    longitude: "36.29520565896303",
+  ),
+  MedicalCenter(
+    name: "مشفى الشامي",
+    phoneNumbers: ["01850213", "01850216"],
+    location: "Damascus Elias",
+    latitude: "33.52005734925068",
+    longitude: "36.268297823256106",
+  ),
+  MedicalCenter(
+    name: "مشفى أمية",
+    phoneNumbers: ["01851773", "01851040", "01853456", "01821671", "01853407"],
+    location: "Damascus",
+    latitude: "33.529321312869726",
+    longitude: "36.29280428234375",
+  ),
+  MedicalCenter(
+    name: "Fouad Khoury Hospital",
+    phoneNumbers: ["01742140", "01344881", "01344882", "01742143", "01742147"],
+    location: "Damascus",
+    latitude: "35.484137",
+    longitude: "33.898572",
+  ),
+  MedicalCenter(
+    name: "مشفى الروضة الجراحي",
+    phoneNumbers: ["01632360", "01632091", "01632707"],
+    location: "Damascus",
+    latitude: "33.52059439031939",
+    longitude: "36.28540908595437",
+  ),
+];
